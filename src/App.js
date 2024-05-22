bd005573d5717f65b26e8f2b284f528af9cf343e
AlinaKulakovska <103748970+AlinaKulakovska@users.noreply.github.com>
2024-05-22 11:34:44 +0300

    .

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ import zoomap from './images/alinakulakovska.github.io_Zoo_Map.html.png'
 import watches from './images/alinakulakovska.github.io_Productshop_.png'
 import watchesbascket from './images/alinakulakovska.github.io_Productshop_busket.html.png'
 import watchesbuy from './images/alinakulakovska.github.io_Productshop_buy.html (1).png'
+import activemain from './images/ActiveLifeLOGmain.png'
+import activeplan from './images/ActiveLifeLOGplan.png'
+import activetracker from './images/ActiveLifeLOGtracker.png'
+import activeexercises from './images/ActiveLifeLOGexercises.png'
 
 import React from "react";
 import { useState, useContext } from "react";
@@ -101,6 +105,37 @@ function App() {
           <h2 className="text-4xl py-2   text-center text-[#F6C4C4] font-medium md:text-6xl mt-24 mb-6">
             Recent work
           </h2>
+
+          <div className="flex justify-between items-center flex-col md:flex-row my-4">
+            <ScrollAnimation animateIn='bounceInLeft'
+              animateOut='bounceOutRight'>
+              <div className="ml-0 md:mr-12 rounded-xl my-4 text-gray-800">
+
+                <a href="https://alinakulakovska.github.io/ActiveLifeLOG/" >
+                  <h3 className="text-right text-2xl m-0 p-0 md:text-4xl italic underline hover:no-underline ">
+                    {languageData.activesitename}
+                  </h3> </a>
+                <div className="text-md py-5 text-justify">
+                  {languageData.activesitedesc}
+                </div>
+              </div>
+            </ScrollAnimation>
+            <Slider {...settings} className="mb-16 w-full md:w-2/5">
+              <div className="max-h-96 overflow-y-auto  rounded-lg  ">
+                <img className="rounded-lg " src={activemain} alt="activelifelog" />
+              </div>
+              <div className="max-h-96 overflow-y-auto rounded-lg  ">
+                <img className="rounded-lg" src={activeexercises} alt="activelifelog" />
+              </div>
+              <div className="max-h-96 overflow-y-auto rounded-lg  ">
+                <img className="rounded-lg" src={activeplan} alt="activelifelog" />
+              </div>
+              <div className="max-h-96 overflow-y-auto rounded-lg  ">
+                <img className="rounded-lg" src={activetracker} alt="activelifelog" />
+              </div>
+            </Slider>
+          </div>
+
           <div className="flex justify-between items-center flex-col md:flex-row my-4">
             <div className="flex justify-center mb-4 rounded-lg shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]">
               <img className="object-fit max-w-full sm:max-w-xs lg:max-w-md rounded-lg " src={thrive} alt="website" />
@@ -111,14 +146,12 @@ function App() {
 
                 <div className="text-md py-5 text-justify text-gray-800 ">
 
-                  <div class="item">
-                    <div class="inner">
-                      <a href="https://alinakulakovska.github.io/Thrive/" >
-                        <h3 className="text-right text-2xl m-0 p-0 md:text-4xl italic underline hover:no-underline"> {languageData.zoositename}</h3>
-                      </a>
-                      {languageData.zoositedesc}
-                    </div>
-                  </div>
+
+                  <a href="https://alinakulakovska.github.io/Thrive/" >
+                    <h3 className="text-right text-2xl m-0 p-0 md:text-4xl italic underline hover:no-underline"> {languageData.zoositename}</h3>
+                  </a>
+                  {languageData.zoositedesc}
+
                 </div>
               </div>
             </ScrollAnimation>
@@ -128,18 +161,8 @@ function App() {
             <ScrollAnimation animateIn='bounceInLeft'
               animateOut='bounceOutRight'>
               <div className="ml-0 md:mr-12 text-justify p-4 rounded-xl text-gray-800">
-
-
-                <div class="item">
-                  <div class="inner">
-                    <a href="https://toyhou.se/_AngeLux_/characters/folder:4475338" > <h3 className="text-right text-2xl m-0 p-0 md:text-4xl italic underline hover:no-underline">                    {languageData.toyname}</h3> </a>
-                    {languageData.toydesc}
-                  </div>
-                </div>
-
-               
-
-
+                <a href="https://toyhou.se/_AngeLux_/characters/folder:4475338" > <h3 className="text-right text-2xl m-0 p-0 md:text-4xl italic underline hover:no-underline">                    {languageData.toyname}</h3> </a>
+                {languageData.toydesc}
               </div>
 
             </ScrollAnimation>
@@ -177,18 +200,16 @@ function App() {
             <ScrollAnimation animateIn='bounceInRight'
               animateOut='bounceOutLeft'>
               <div className=" rounded-xl text-gray-800  ">
-                <div class="item">
-                  <div class="inner">
-                    <a href="https://alinakulakovska.github.io/Zoo/" > <h3 className="text-right text-2xl m-0 p-0 md:text-4xl italic underline hover:no-underline ">Zoo website Project</h3> </a>
-                    <div className="text-md py-5 text-justify ">
-                      <p className="mt-2"><b>Price Rate Conversion via API: </b> Real-time price rate conversion via API ensures that visitors from around the globe can easily understand ticket prices, facilitating a seamless booking experience.</p>
-                      <p className="mt-2"><b>Map with Animal Description: </b> An interactive map feature guides users through the zoo's layout while providing insightful descriptions of each animal exhibit, enriching the visitor's journey with educational content.</p>
-                      <p className="mt-2"><b>Ticket Purchase Page:</b> with input from which sends out email with purchase information</p>
-                      <p className="mt-2"><b>Carousel of Images:</b>  A visually stunning carousel showcases captivating images of various animals, offering visitors a glimpse into the beauty and diversity of the zoo's inhabitants.</p>
-                      <p className="mt-2"><b>Modal with animation: </b></p>
-                      <p className="mt-2"><b>For this project i used JS, HTML, CSS</b></p>
-                    </div>
-                  </div>
+
+                <a href="https://alinakulakovska.github.io/Zoo/" > <h3 className="text-right text-2xl m-0 p-0 md:text-4xl italic underline hover:no-underline ">Zoo website Project</h3> </a>
+                <div className="text-md py-5 text-justify ">
+                  <p className="mt-2"><b>Price Rate Conversion via API: </b> Real-time price rate conversion via API ensures that visitors from around the globe can easily understand ticket prices, facilitating a seamless booking experience.</p>
+                  <p className="mt-2"><b>Map with Animal Description: </b> An interactive map feature guides users through the zoo's layout while providing insightful descriptions of each animal exhibit, enriching the visitor's journey with educational content.</p>
+                  <p className="mt-2"><b>Ticket Purchase Page:</b> with input from which sends out email with purchase information</p>
+                  <p className="mt-2"><b>Carousel of Images:</b>  A visually stunning carousel showcases captivating images of various animals, offering visitors a glimpse into the beauty and diversity of the zoo's inhabitants.</p>
+                  <p className="mt-2"><b>Modal with animation: </b></p>
+                  <p className="mt-2"><b>For this project i used JS, HTML, CSS</b></p>
+
                 </div>
               </div>
             </ScrollAnimation>
@@ -199,8 +220,7 @@ function App() {
             <ScrollAnimation animateIn='bounceInLeft'
               animateOut='bounceOutRight'>
               <div className="ml-0 md:mr-12 rounded-xl my-4 text-gray-800">
-              <div class="item">
-                  <div class="inner">
+
                 <a href="https://alinakulakovska.github.io/Productshop/" > <h3 className="text-right text-2xl m-0 p-0 md:text-4xl italic underline hover:no-underline ">E-Commerce Website Project: Smart-Watch Store</h3> </a>
                 <div className="text-md py-5 text-justify">
                   <p className="mt-2"><b>Flowy Front Page: </b> Real-time price rate conversion via API ensures that visitors from around the globe can easily understand ticket prices, facilitating a seamless booking experience.</p>
@@ -209,9 +229,8 @@ function App() {
                   <p className="mt-2"><b>Checkout Page:</b>  Seamlessly transition from product selection to checkout with the user-friendly checkout page. Here, customers can review their selected items, view the total cost, and proceed to finalize their purchase with confidence.
                   </p>
                   <p className="mt-2"><b>For this project i used JS, HTML, CSS</b></p>
+
                 </div>
-              </div>
-              </div>
               </div>
             </ScrollAnimation>
             <Slider {...settings} className="mb-16 w-full md:w-2/5">
@@ -234,9 +253,8 @@ function App() {
             <ScrollAnimation animateIn='bounceInRight'
               animateOut='bounceOutLeft'>
               <div className="ml-0 md:ml-12 rounded-xl text-gray-800">
-              <div class="item">
-                  <div class="inner">
-               <a href="https://alinakulakovska.github.io/QuizAppReact/"><h3 className="text-right text-2xl m-0 p-0 md:text-4xl italic  ">Quiz App with Timer using React</h3></a> 
+
+                <a href="https://alinakulakovska.github.io/QuizAppReact/"><h3 className="text-right text-2xl m-0 p-0 md:text-4xl italic  underline hover:no-underline">Quiz App with Timer using React</h3></a>
                 <div className="text-md py-5 text-justify">
                   <p>
                     This Quiz App with Timer using React offers a fun and immersive way to test your knowledge while sharpening your cognitive skills </p>
@@ -244,9 +262,9 @@ function App() {
                   <p className="mt-2"><b>Questions with Images:</b> </p>
                   <p className="mt-2"><b>Multiple-Point Questions:</b> </p>
                   <p className="mt-2"><b>Timer: </b> </p>
+
+
                 </div>
-              </div>
-              </div>
               </div>
             </ScrollAnimation>
           </div>
--- src/languageContext.js
+++ src/languageContext.js
@@ -7,9 +7,9 @@ let en = {
     "description": "Building things for the web has always been exciting to me. It's like taking a blank canvas and turning it into something interactive, visually appealing, and user-friendly. As a front-end developer, I focus on the user-facing side of websites and applications. I love the challenge of bringing those elements together to create a seamless and enjoyable experience for users. One of my strengths is being a fast learner. The tech world is constantly evolving, and I'm always eager to pick up new skills and frameworks. ",
     "zoositename": "Thrive Recipes Practice website",
     "zoositedesc": <div>
- <p>
- This is a practice responsive website where I've integrated a variety of features to demonstrate my proficiency in web development.
-                  </p>
+        <p>
+            This is a practice responsive website where I've integrated a variety of features to demonstrate my proficiency in web development.
+        </p>
         <p className="mt-2"><b>Page Pagination:</b>  I've implemented page pagination which shows 3 recipes per page. </p>
         <p className="mt-2"><b>API Fetching Random Meal:</b> I've integrated an API fetching feature that displays random meal suggestions.</p>
         <p className="mt-2"><b>Sorting Recipes:</b> </p>
@@ -19,21 +19,36 @@ let en = {
         <p className="mt-2"><b>Local Storage for Parsing Data:</b>  I've utilized local storage to store data of clicked recipes.</p>
         <p className="mt-2"><b>For this project i used JS, HTML, CSS</b></p>
     </div>,
-        "toyname":"Codes for sale",
-        "toydesc":<div>
-                 <p className="text-md py-5  ">
-                      Within this collection, you'll find a series of meticulously crafted codes designed specifically for selling templates on Toyhouse. These templates serve as an indispensable tool for designers seeking to vividly portray their characters and worlds on the Toyhouse platform.
-                    </p>
-                    <ul className="list-disc ml-4"><b>Technologies Utilized:</b>
-                      <li className="mt-2">Bootstrap: Leveraging the power and flexibility of Bootstrap framework, I ensured seamless responsiveness and dynamic functionality across various devices and screen sizes.
-                      </li>
-                      <li className="mt-2">HTML: HTML was expertly employed to structure the content, ensuring clarity and accessibility.</li>
-                      <li className="mt-2">CSS: With CSS, I breathed life into the designs, meticulously crafting styles to enhance visual appeal and user experience.</li>
-                      <li>Figma: Prior to coding, meticulous design work was carried out in Figma, laying the groundwork for visually stunning and intuitive interfaces.
-                      </li>
-                    </ul>
+    "toyname": "Codes for sale",
+    "toydesc": <div>
+        <p className="text-md py-5  ">
+            Within this collection, you'll find a series of meticulously crafted codes designed specifically for selling templates on Toyhouse. These templates serve as an indispensable tool for designers seeking to vividly portray their characters and worlds on the Toyhouse platform.
+        </p>
+        <ul className="list-disc ml-4"><b>Technologies Utilized:</b>
+            <li className="mt-2">Bootstrap: Leveraging the power and flexibility of Bootstrap framework, I ensured seamless responsiveness and dynamic functionality across various devices and screen sizes.
+            </li>
+            <li className="mt-2">HTML: HTML was expertly employed to structure the content, ensuring clarity and accessibility.</li>
+            <li className="mt-2">CSS: With CSS, I breathed life into the designs, meticulously crafting styles to enhance visual appeal and user experience.</li>
+            <li>Figma: Prior to coding, meticulous design work was carried out in Figma, laying the groundwork for visually stunning and intuitive interfaces.
+            </li>
+        </ul>
 
-        </div>,
+    </div>,
+    "activesitename": "Workout tracker/planer, Workouts collection",
+    "activesitedesc": <div>
+        <p>ActiveLifeLOG is a comprehensive fitness tracking platform designed to empower users in their journey towards a healthier lifestyle. Through a seamless integration of innovative features and intuitive design.</p>
+        <p className="mt-2"><b>Authentication Process with Firebase: </b>Ensure a secure and streamlined user experience with Firebase authentication, allowing users to register, log in effortlessly.</p>
+        <p className="mt-2"><b>Tracker Calendar: </b> Stay organized and motivated with a dynamic tracker calendar, powered by Firebase Realtime Database. Users can easily schedule and log their workouts, creating a personalized fitness routine tailored to their needs.
+        </p>
+        <p className="mt-2"><b>Workout Collection:</b>  Access an extensive collection of curated workouts, categorized based on duration, intensity, and fitness goals. Users can filter workouts to suit their preferences or opt for a random selection to keep their routines exciting and varied.
+        </p>
+        <p className="mt-2"><b>Workout Details:</b>  Dive deeper into specific workouts with detailed descriptions, accompanied by Spotify embedded playlists, YouTube workout videos, or structured lists of exercises with timers. This feature enhances user engagement by providing multimedia resources to support their workout sessions.
+        </p>
+        <p className="mt-2"><b>Landing Page:</b>  Engage users from the outset with a visually appealing and informative landing page that highlights the core functionalities of ActiveLifeLOG.
+
+        </p>
+        <p className="mt-2"><b>For this project i used React, Js, HTML, Tailwind, Sass, Firebase</b></p>
+    </div>,
 }
 let ua = {
     "name": "Аліна Кулаковська",
@@ -41,9 +56,9 @@ let ua = {
     "description": "Створювати вебсайти завжди було для мене цікавим заняттям. Це як взяти чисте полотно і перетворити його на щось інтерактивне, візуально привабливе та зручне для користувача. Як фронтенд-розробник, я зосереджуюсь на користувацькій стороні веб-сайтів та додатків. Мені подобається завдання об'єднати ці елементи разом, щоб створити безперебійний і приємний досвід для користувачів. Однією з моїх сильних сторін є те, що я швидко навчаюсь. Світ технологій постійно розвивається, і я завжди прагну здобути нові навички та фреймворки. ",
     "zoositename": "Thrive - Сайт рецептів",
     "zoositedesc": <div>
-         <p>
-         Це практичний адаптивний веб-сайт, де я інтегрував різноманітні функції, щоб продемонструвати свою майстерність у веб-розробці.
-                  </p>
+        <p>
+            Це практичний адаптивний веб-сайт, де я інтегрував різноманітні функції, щоб продемонструвати свою майстерність у веб-розробці.
+        </p>
         <p className="mt-2"><b>Пагінація сторінок:</b> яка показує по 3 рецепти на сторінці. </p>
         <p className="mt-2"><b>API Отримання випадкового рецепту:</b>  функція отримання даних через API, яка відображає випадкові пропозиції страв.</p>
         <p className="mt-2"><b>Сортування рецептів:</b></p>
@@ -53,19 +68,33 @@ let ua = {
         <p className="mt-2"><b>Локальне сховище для парсингу даних:</b> Я використовувала локальне сховище для зберігання даних про натиснуті рецепти.</p>
         <p className="mt-2"><b>Для цього проекту я використовувала JS, HTML, CSS</b></p>
     </div>,
-    "toyname":"Коди на продаж",
-    "toydesc":<div>
-    <p className="text-md py-5  ">
-    У цій колекції ви знайдете серію ретельно розроблених кодів, створених спеціально для продажу шаблонів на Toyhouse. Ці шаблони слугують незамінним інструментом для дизайнерів, які прагнуть яскраво зобразити своїх персонажів та світи на платформі Toyhouse.
-                    </p>
-                    <ul className="list-disc ml-4"><b>Використані технології:</b>
-                      <li className="mt-2">Bootstrap: Використовуючи потужність і гнучкість фреймворку Bootstrap, я забезпечила бездоганну адаптивність і динамічну функціональність на різних пристроях і з різними розмірами екранів.
-                      </li>
-                      <li className="mt-2">HTML: Для структурування контенту було майстерно використано HTML, що забезпечило чіткість і доступність.</li>
-                      <li className="mt-2">CSS: За допомогою CSS я вдихнула життя в дизайн, ретельно розробляючи стилі, щоб покращити візуальну привабливість та зручність користування.</li>
-                      <li>Figma: Перед кодуванням була проведена ретельна робота над дизайном у Figma, яка заклала основу для візуально приголомшливих та інтуїтивно зрозумілих інтерфейсів.
-                      </li>
-                    </ul>
+    "toyname": "Коди на продаж",
+    "toydesc": <div>
+        <p className="text-md py-5  ">
+            У цій колекції ви знайдете серію ретельно розроблених кодів, створених спеціально для продажу шаблонів на Toyhouse. Ці шаблони слугують незамінним інструментом для дизайнерів, які прагнуть яскраво зобразити своїх персонажів та світи на платформі Toyhouse.
+        </p>
+        <ul className="list-disc ml-4"><b>Використані технології:</b>
+            <li className="mt-2">Bootstrap: Використовуючи потужність і гнучкість фреймворку Bootstrap, я забезпечила бездоганну адаптивність і динамічну функціональність на різних пристроях і з різними розмірами екранів.
+            </li>
+            <li className="mt-2">HTML: Для структурування контенту було майстерно використано HTML, що забезпечило чіткість і доступність.</li>
+            <li className="mt-2">CSS: За допомогою CSS я вдихнула життя в дизайн, ретельно розробляючи стилі, щоб покращити візуальну привабливість та зручність користування.</li>
+            <li>Figma: Перед кодуванням була проведена ретельна робота над дизайном у Figma, яка заклала основу для візуально приголомшливих та інтуїтивно зрозумілих інтерфейсів.
+            </li>
+        </ul>
+    </div>,
+    "activesitename": "Трекер/планувальник тренувань, колекція тренувань",
+    "activesitedesc": <div>
+        <p>ActiveLifeLOG - це комплексна платформа для фітнес-трекінгу, розроблена, щоб розширити можливості користувачів на шляху до здорового способу життя. Завдяки безшовній інтеграції інноваційних функцій та інтуїтивно зрозумілому дизайну.</p>
+        <p className="mt-2"><b>Процес автентифікації з Firebase: </b>Забезпечте безпечну та спрощену роботу користувачів з автентифікацією Firebase, дозволяючи користувачам реєструватися та входити в систему без зайвих зусиль.</p>
+        <p className="mt-2"><b>Трекер з календарем: </b> Залишайтеся організованими та мотивованими за допомогою динамічного календаря трекера, що працює на основі бази даних Firebase Realtime Database. Users can easily schedule and log their workouts, creating a personalized fitness routine tailored to their needs.
+        </p>
+        <p className="mt-2"><b>Колекція тренувань:</b>  Доступ до великої колекції кураторських тренувань, класифікованих за тривалістю, інтенсивністю та фітнес-цілями. Користувачі можуть фільтрувати тренування відповідно до своїх уподобань або вибрати випадковий вибір, щоб урізноманітнити свої тренування.
+        </p>
+        <p className="mt-2"><b>Деталі тренування:</b>  Занурюйтесь у конкретні тренування з детальними описами, що супроводжуються вбудованими плейлистами Spotify, відеозаписами тренувань на YouTube або структурованими списками вправ з таймерами. Ця функція підвищує залученість користувачів, надаючи мультимедійні ресурси для підтримки їхніх тренувань.
+        </p>
+        <p className="mt-2"><b>Лендінг:</b>  Залучайте користувачів з самого початку за допомогою візуально привабливої та інформативної цільової сторінки, яка висвітлює основні функціональні можливості ActiveLifeLOG.
+        </p>
+        <p className="mt-2"><b>Для цього проекту я використовував React, Js, HTML, Tailwind, Sass, Firebase</b></p>
     </div>,
 
 }
